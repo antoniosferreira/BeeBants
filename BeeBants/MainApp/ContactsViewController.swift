d6b1389a2b6747f1dbfa30670aaa8b4be0acd596
António Ferreira <antoniosferreira@me.com>
2020-06-28 13:30:49 +0100

    Last version

--- BeeBants/MainApp/ContactsViewController.swift
+++ BeeBants/MainApp/ContactsViewController.swift
@@ -10,13 +10,38 @@ import UIKit
 
 class ContactsViewController: UIViewController {
 
+    @IBOutlet weak var mail: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
+    @IBAction func mailTapped(_ sender: Any) {
+        let url = URL.init(string: "mailto:beebants@gmail.com?ContactFromApp-")
+                       guard let link = url, UIApplication.shared.canOpenURL(link) else { return }
+                       UIApplication.shared.open(link)
+    }
+    
+    
+    @IBAction func instaTapped(_ sender: Any) {
+    let url = URL.init(string: "https://www.instagram.com/bee.bants/")
+                          guard let link = url, UIApplication.shared.canOpenURL(link) else { return }
+                          UIApplication.shared.open(link)
+    }
+    
+    @IBAction func fbTapped(_ sender: Any) {
+        let url = URL.init(string: "https://www.facebook.com/beebants/")
+        guard let link = url, UIApplication.shared.canOpenURL(link) else { return }
+        UIApplication.shared.open(link)
+    }
+    
+    @IBAction func webTapped(_ sender: Any) {
+        let url = URL.init(string: "https://beebants.wixsite.com/website")
+        guard let link = url, UIApplication.shared.canOpenURL(link) else { return }
+        UIApplication.shared.open(link)
+    }
     /*
     // MARK: - Navigation
 
